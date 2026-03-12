e452ed2b7f21035221502ba322b39f95430663af
Kosma <oversider.kosma@gmail.com>
2026-03-12 19:21:38 +0300

    Build adjustments

--- build.bat
+++ build.bat
@@ -1,27 +1,29 @@
 @echo off
 echo Build started at %date% %time%
-chcp 437
+setlocal enabledelayedexpansion
+
+chcp 65001
 set LANG=en_US.UTF-8
 set LC_ALL=C
 
-uv sync
+set NUITKA_CACHE_DIR=.nuitka_cache
 
-uv run utils.py bumb
+uv sync || goto :error
+uv run utils.py bumb || goto :error
 
-FOR /F "tokens=*" %%a IN ('uv run utils.py get_version') DO SET VERSION=%%a
-FOR /F "tokens=*" %%a IN ('uv run utils.py get_name') DO SET APPNAME=%%a
-FOR /F "tokens=*" %%a IN ('uv run utils.py get_description') DO SET DESCRIPTION=%%a
-
+for /f "usebackq tokens=*" %%a in (`uv run utils.py get_version`) do set "VERSION=%%a"
+for /f "usebackq tokens=*" %%a in (`uv run utils.py get_name`) do set "APPNAME=%%a"
+for /f "usebackq tokens=*" %%a in (`uv run utils.py get_description`) do set "DESCRIPTION=%%a"
 
 uv run nuitka ^
     --standalone ^
     --onefile ^
     --product-version=%VERSION% ^
     --file-version=%VERSION% ^
-    --product-name=%APPNAME% ^
+    --product-name="%APPNAME%" ^
     --file-description="%DESCRIPTION%" ^
     --onefile-tempdir-spec="{CACHE_DIR}/{PRODUCT}/{VERSION}" ^
-    --jobs=%NUMBER_OF_PROCESSORS%^
+    --jobs=%NUMBER_OF_PROCESSORS% ^
     --assume-yes-for-downloads ^
     --include-data-dir=frontend=frontend ^
     --include-data-dir=vendor=vendor ^
@@ -31,6 +33,14 @@ uv run nuitka ^
     --onefile-windows-splash-screen-image="misc/splashscreen.png" ^
     --windows-icon-from-ico=frontend/static/favicon.ico ^
     --user-plugin=prepare_build.py ^
-    --output-filename=%APPNAME%.exe ^
+    --output-dir=dist ^
+    --output-filename="%APPNAME%_v%VERSION%.exe" ^
     main.py
+
+if %errorlevel% neq 0 goto :error
 echo Build finished at %date% %time%
+exit /b 0
+
+:error
+echo Build FAILED at %date% %time% with error %errorlevel%
+exit /b %errorlevel%
