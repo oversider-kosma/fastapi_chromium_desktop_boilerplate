febc24a05bb5faf860593b6b9f544ae331f3026d
Kosma <oversider.kosma@gmail.com>
2026-03-13 18:51:48 +0300

    feat(server): allow running backend server independently for debugging purposes

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
+import argparse
 import threading
 import uvicorn
 from backend import app
+from utils import get_free_port
 
 class AppServer(uvicorn.Server):
     def install_setup(self) -> None:
@@ -21,3 +23,17 @@ def run_server(port) -> None:
     current_thread = threading.current_thread()
     setattr(current_thread,"server", server)
     server.run()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument('--port', type=int)
+    args = parser.parse_args()
+
+    port = args.port or get_free_port()
+    print(f"Running debug server at 127.0.0.1:{port}...")
+    print(f"Press Ctrl-C to quit.")
+    try:
+        run_server(port)
+    except KeyboardInterrupt:
+        print()
