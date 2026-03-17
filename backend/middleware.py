33dbd7106fcd740bccd0367de640c330508ce616
Kosma <oversider.kosma@gmail.com>
2026-03-17 10:25:02 +0300

    fix (backend): remove erroneous extra call `call_next` in `NoCacheMiddleware`

--- backend/middleware.py
+++ backend/middleware.py
@@ -9,7 +9,6 @@ class NoCacheMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request,
                        call_next: Callable[[Request], Response]) -> Response:
         response: Response = await call_next(request)
-        response = await call_next(request)
         response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
         response.headers["Pragma"] = "no-cache"
         response.headers["Expires"] = "0"
