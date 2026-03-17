7d4d0dc2980ef3e9ed3bbccc267fefdc0911713e
Kosma <oversider.kosma@gmail.com>
2026-03-17 07:11:19 +0300

    feat (backend): NoCacheMiddleware

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
+from backend.middleware import NoCacheMiddleware
+
 from .constants import STATIC_DIR
 from .routes import system, static
 
+
 app: FastAPI = FastAPI()
+
+# no actual need for caching when server and client interacts through localhost loopback
+app.add_middleware(NoCacheMiddleware)
 
 app.include_router(system.router)
 app.include_router(static.router)
---
+++ backend/middleware.py
@@ -0,0 +1,16 @@
+from typing import Callable
+
+from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.requests import Request
+from starlette.responses import Response
+
+
+class NoCacheMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request,
+                       call_next: Callable[[Request], Response]) -> Response:
+        response: Response = await call_next(request)
+        response = await call_next(request)
+        response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
+        response.headers["Pragma"] = "no-cache"
+        response.headers["Expires"] = "0"
+        return response
